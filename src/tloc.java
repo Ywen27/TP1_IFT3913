b74a7e1305bbd36188da01214683a3d86cd08089
Wen Yin <andywenyin@gmail.com>
2023-09-22 17:38:23 -0400

    Create tloc.java

---
+++ src/tloc.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class tloc {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Tloc <path_to_java_file>");
+            System.exit(1);
+        }
+
+        String filePath = args[0];
+        try {
+            int tloc = calculateTloc(filePath);
+            System.out.println(tloc);
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    public static int calculateTloc(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            int tloc = 0;
+            boolean inCommentBlock = false;
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                if (line.startsWith("/*")) {
+                    inCommentBlock = true;
+                }
+
+                if (!line.isEmpty() && !line.startsWith("//") && !inCommentBlock) {
+                    tloc++;
+                }
+
+                if (line.endsWith("*/")) {
+                    inCommentBlock = false;
+                }
+            }
+
+            return tloc;
+        }
+    }
+}
