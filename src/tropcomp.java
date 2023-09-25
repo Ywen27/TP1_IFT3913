7ba7c62626f94dcfe766fdafbcfa976f97a65aef
yass303 <jimmy.yassin.hassanaly@umontreal.ca>
2023-09-25 14:01:31 -0400

    Update tropcomp.java

--- src/tropcomp.java
+++ src/tropcomp.java
@@ -7,7 +7,20 @@ import java.util.function.ToDoubleFunction;
 public class tropcomp {
 	
 	public static void main(String[] args) throws Exception {
-		
+		if (args.length < 2 || args.length > 4 || (args.length == 3 && !args[0].equals("-o"))) {
+            System.out.println("Usage: java tropcomp [-o <path_to_output_file.csv>] <path_to_maven_project> " +
+                    "<threshold_percentage>");
+            System.exit(1);
+        }
+
+        String outputPath = null;
+        int inputIndex = 0;
+        if (args[0].equals("-o")) {
+            outputPath = args[1];
+            inputIndex = 2;
+        }
+        String projectPath = args[inputIndex];
+        double threshold = Double.parseDouble(args[inputIndex + 1]);
 	};
 	
 	private static class ClassMetrics {
